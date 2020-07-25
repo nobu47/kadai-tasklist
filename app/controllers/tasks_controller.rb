7d10e9d42c0864d49b18e3d01325407423f9e8dc
nobu <nkomiya.m47@gmail.com>
2020-07-25 04:20:16 +0000

    task-user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+    @tasks = Task.order(id: :desc).page(params[:page]).per(15)
   end
 
   def show
@@ -13,12 +15,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -55,4 +58,12 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
